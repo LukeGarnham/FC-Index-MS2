9bf9ad730dcb6e3f95686c6cb3f5fde1c8d7e38b
LukeGarnham <lukegarnham@msn.com>
2020-10-22 16:52:41 +0000

    Tidied some of the search-form section styling inc search box and button.  Started building the API request in the footballAPI.js file.

--- assets/js/footballAPI.js
+++ assets/js/footballAPI.js
@@ -26,11 +26,11 @@ document.getElementById("report-bug-button").addEventListener("mouseout", functi
 });
 
 // Change button to grey when clicked
-// This will also need to launch modal to email query - TO BE BUILT
+// This will also need to launch modal to email query - TO BE BUILT - once done the button will return to blue
 document.getElementById("report-bug-button").addEventListener("click", function getElement() {
     let el = document.getElementById("report-bug-button");
-    button.classList.remove("blue");
-    button.classList.add("grey");
+    el.classList.remove("blue");
+    el.classList.add("grey");
 });
 
 function addShadow(button) {
@@ -40,3 +40,24 @@ function addShadow(button) {
 function removeShadow(button) {
     button.classList.remove("button-shadow");
 };
+
+// ----------------------------------------------------------------------------------------------------------Extract API data
+// The below xhr request was orginally copied from the API documentation but then amended for the purposes of this project:
+// https://rapidapi.com/api-sports/api/api-football/endpoints
+const data = null;
+
+const xhr = new XMLHttpRequest();
+xhr.withCredentials = true;
+
+xhr.addEventListener("readystatechange", function () {
+	if (this.readyState === this.DONE) {
+		console.log(this.responseText);
+	}
+});
+
+xhr.open("GET", "https://rapidapi.p.rapidapi.com/v2/teams/search/real_m");
+xhr.setRequestHeader("x-rapidapi-host", "api-football-v1.p.rapidapi.com");
+xhr.setRequestHeader("x-rapidapi-key", "03db490835mshd5eaf0436ca3429p1687b6jsnf4663d1db28c");
+
+xhr.send(data);
+
