71f51b5974de2b0f410df1999844a86c1f84bde3
LukeGarnham <lukegarnham@msn.com>
2020-10-22 16:12:37 +0000

    Added content to the search-form section.  Modified JS event listeners for the button in the footer and added them to the Search button.

--- assets/js/footballAPI.js
+++ assets/js/footballAPI.js
@@ -1,19 +1,42 @@
+// ----------------------------------------------------------------------------------------------------------Event listeners for buttons
+// ------------------------------------------------Club Search Button
+// Add shadow effect on mouse over
+document.getElementById("club-search-button").addEventListener("mouseover", function getElement() {
+    let el = document.getElementById("club-search-button");
+    addShadow(el);
+});
+
+// Add shadow effect on mouse off
+document.getElementById("club-search-button").addEventListener("mouseout", function getElement() {
+    let el = document.getElementById("club-search-button");
+    removeShadow(el);
+});
+
+// ------------------------------------------------Report Bug Button in footer
+// Add shadow effect on mouse over
 document.getElementById("report-bug-button").addEventListener("mouseover", function getElement() {
     let el = document.getElementById("report-bug-button");
-    buttonHoverOn(el);
+    addShadow(el);
 });
 
+// Add shadow effect on mouse off
 document.getElementById("report-bug-button").addEventListener("mouseout", function getElement() {
     let el = document.getElementById("report-bug-button");
-    buttonHoverOff(el);
+    removeShadow(el);
 });
 
-function buttonHoverOn(button) {
-    button.classList.remove("grey");
-    button.classList.add("blue");
-}
-
-function buttonHoverOff(button) {
+// Change button to grey when clicked
+// This will also need to launch modal to email query - TO BE BUILT
+document.getElementById("report-bug-button").addEventListener("click", function getElement() {
+    let el = document.getElementById("report-bug-button");
     button.classList.remove("blue");
-    button.classList.add("grey");   
-}
+    button.classList.add("grey");
+});
+
+function addShadow(button) {
+    button.classList.add("button-shadow");
+};
+
+function removeShadow(button) {
+    button.classList.remove("button-shadow");
+};
