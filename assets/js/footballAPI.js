2b3e0f6e39758e2a40d1bcdcc2d1483d289e1f21
LukeGarnham <lukegarnham@msn.com>
2020-10-23 11:54:59 +0000

    Begun unpacking API data.  Managed to console log the results of a seach successfully.  Need to unpack into the html content still.

--- assets/js/footballAPI.js
+++ assets/js/footballAPI.js
@@ -12,6 +12,9 @@ document.getElementById("club-search-button").addEventListener("mouseout", funct
     removeShadow(el);
 });
 
+// Search for club and call the API via the teamSearch function
+document.getElementById("club-search-button").addEventListener("click", teamSearch);
+
 // ------------------------------------------------Report Bug Button in footer
 // Add shadow effect on mouse over
 document.getElementById("report-bug-button").addEventListener("mouseover", function getElement() {
@@ -41,23 +44,50 @@ function removeShadow(button) {
     button.classList.remove("shadow-effect");
 };
 
+
+
+
 // ----------------------------------------------------------------------------------------------------------Extract API data
 // The below xhr request was orginally copied from the API documentation but then amended for the purposes of this project:
 // https://rapidapi.com/api-sports/api/api-football/endpoints
-// const data = null;
 
-// const xhr = new XMLHttpRequest();
-// xhr.withCredentials = true;
+function getAPIData(clubName, cb) {
 
-// xhr.addEventListener("readystatechange", function () {
-// 	if (this.readyState === this.DONE) {
-// 		console.log(this.responseText);
-// 	}
-// });
+    const xhr = new XMLHttpRequest();
+    xhr.withCredentials = true;
 
-// xhr.open("GET", "https://rapidapi.p.rapidapi.com/v2/teams/search/real_m");
-// xhr.setRequestHeader("x-rapidapi-host", "api-football-v1.p.rapidapi.com");
-// xhr.setRequestHeader("x-rapidapi-key", "03db490835mshd5eaf0436ca3429p1687b6jsnf4663d1db28c");
+    xhr.open("GET", "https://rapidapi.p.rapidapi.com/v2/teams/search/" + clubName);
+    xhr.setRequestHeader("x-rapidapi-host", "api-football-v1.p.rapidapi.com");
+    xhr.setRequestHeader("x-rapidapi-key", "03db490835mshd5eaf0436ca3429p1687b6jsnf4663d1db28c");
+    xhr.send();
 
-// xhr.send(data);
+    xhr.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            cb(JSON.parse(this.responseText));
+        };
+    };
+};
+
+function teamSearch() {
+    // Get the search string from the search input box
+    let searchString = document.getElementById("club-search").value;
+    // Replace any spaces with underscores and make string all lower case
+    searchString = searchString.replace(/ /g, "_").toLowerCase();
+    // Enter the search string into the getAPIData function and create another function to do something with the results.
+    getAPIData(searchString, function(apiResults) {
+        // Reset the error message and results to be empty
+
+        // document.getElementById("error-message").innerHTML = "";
+        // document.getElementById("results-table").innerHTML = "";
+
+        if (apiResults.api.results == 0) {
+            document.getElementById("error-message").innerHTML = "Sorry, no teams found.  Please check the spelling or try searching for a different team.";
+        } else {
+            console.log(apiResults);
+        }
+
+        // Unhide the search-results section.
+        document.getElementById("search-results").classList.remove("hide");
+    });
+};
 
