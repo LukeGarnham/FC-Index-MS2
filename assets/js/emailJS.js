e926af8edd0a6d269a6a96674b6ad22c244ddcca
LukeGarnham <lukegarnham@msn.com>
2020-11-14 19:27:52 +0000

    Added a loading gif which displays immediately after the user submits the form.

--- assets/js/emailJS.js
+++ assets/js/emailJS.js
@@ -8,14 +8,16 @@ function sendMail(reportBugForm) {
         "club_name": clubName(reportBugForm.clubName.value),
         "bug_details": reportBugForm.bugDescription.value
     })
+    .then(document.getElementById("report-bug-modal-content").classList.add("hide"))
+    .then(document.getElementById("report-loading").classList.remove("hide"))
     .then(
         function(response) {
             document.getElementById("report-bug-form").reset();
-            document.getElementById("report-bug-modal-content").classList.add("hide");
+            document.getElementById("report-loading").classList.add("hide");
             document.getElementById("report-bug-success").classList.remove("hide");
         },
         function(error) {
-            document.getElementById("report-bug-modal-content").classList.add("hide");
+            document.getElementById("report-loading").classList.add("hide");
             document.getElementById("report-bug-failure").classList.remove("hide");
         }
     );    
